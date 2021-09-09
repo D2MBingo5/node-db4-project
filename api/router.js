c34a2b900cd45c08bc4074a2ea58ae5266164232
Leu Family <romanianfobjr@gmail.com>
2021-09-09 17:48:34 -0500

    properly gets recipe by id

--- api/router.js
+++ api/router.js
@@ -2,14 +2,16 @@ const Recipes = require('./model')
 
 const router = require('express').Router()
 
-router.use('*', (req, res, next) => { // eslint-disable-line
-    res.json({ api: 'up' })
+router.get('/:recipe_id', (req, res, next) => {
+    Recipes.getRecipeById(req.params.recipe_id)
+        .then(r => {
+            res.status(200).json(r)
+        })
+        .catch(next)
 })
 
-router.get('/:recipe_id', (req, res, next) => {
-    const { recipe_id } = req.params
-
-
+router.use('*', (req, res, next) => { // eslint-disable-line
+    res.json({ api: 'up' })
 })
 
 module.exports = router
