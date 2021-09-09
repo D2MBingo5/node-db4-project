ff73d4661c5ec511b28ae91d3f5bbe12570190c6
Leu Family <romanianfobjr@gmail.com>
2021-09-09 17:29:57 -0500

    correction to 02 seed

--- data/seeds/02-steps.js
+++ data/seeds/02-steps.js
@@ -5,13 +5,13 @@ exports.seed = function(knex, Promise) {
             step_instructions: 'Grab pickle' },
         { recipe_id: 1, step_number: 2, 
             step_instructions: 'Put pickle in cup' },
-        { recipe_id: 1, step_number: 1, 
+        { recipe_id: 2, step_number: 1, 
             step_instructions: 'Add 1 cup of water to pot on high heat' },
-        { recipe_id: 1, step_number: 2, 
+        { recipe_id: 2, step_number: 2, 
             step_instructions: 'Add 1 box of spaghetti pasta to boiling water' },
-        { recipe_id: 1, step_number: 3, 
+        { recipe_id: 2, step_number: 3, 
             step_instructions: 'Drain water and put cooked pasta on plate' },
-        { recipe_id: 1, step_number: 4, 
+        { recipe_id: 2, step_number: 4, 
             step_instructions: 'Add 1/2 cup of tomato sauce to top of pasta' },
     ]);
 };
