932e6603adf60740058509fe24f9569998d23d37
Leu Family <romanianfobjr@gmail.com>
2021-09-09 17:43:03 -0500

    no longer crashing

--- api/router.js
+++ api/router.js
@@ -1,9 +1,11 @@
 const Recipes = require('./model')
 
-const router = express.Router()
+const router = require('express').Router()
 
 router.get('/:recipe_id', (req, res, next) => {
     const { recipe_id } = req.params
 
 
 })
+
+module.exports = router
