c6d4238f1438e86f126eb8c741f8ced74a6ddc9c
Leu Family <romanianfobjr@gmail.com>
2021-09-09 17:46:04 -0500

    everything properly wired

--- api/router.js
+++ api/router.js
@@ -2,6 +2,10 @@ const Recipes = require('./model')
 
 const router = require('express').Router()
 
+router.use('*', (req, res, next) => { // eslint-disable-line
+    res.json({ api: 'up' })
+})
+
 router.get('/:recipe_id', (req, res, next) => {
     const { recipe_id } = req.params
 
