651695bfac2552df1dfaac5dadf578fddd8056f7
Leu Family <romanianfobjr@gmail.com>
2021-09-09 17:01:59 -0500

    seed files complete

--- data/seeds/04-step_ingredients.js
+++ data/seeds/04-step_ingredients.js
@@ -1,6 +1,9 @@
 
 exports.seed = function(knex, Promise) {
     return knex('step_ingredients').insert([
-        { quantity: 1, ingredient_id: 1, step_id: 1 }
+        { quantity: 1, ingredient_id: 1, step_id: 1 },
+        { quantity: 1, ingredient_id: 2, step_id: 3 },
+        { quantity: 1, ingredient_id: 3, step_id: 4 },
+        { quantity: 0.5, ingredient_id: 4, step_id: 6 },
     ]);
 };
